03f5346061ca4c8927cadc6c64c46bde5a8899d9
Francis <francisjhackenberger@gmail.com>
2019-03-08 15:16:07 -0500

    Updated job title and updated skills to more closely match my current levels of expertise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,21 @@
 jQuery(document).ready(function($) {
+  /*======= Skillset *=======*/
 
-    /*======= Skillset *=======*/
-    
-    $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
+  $(".level-bar-inner").css("width", "0");
 
-        $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
-        });
-
+  $(window).on("load", function() {
+    $(".level-bar-inner").each(function() {
+      const itemWidth = $(this).data("level");
+      $(this).animate({width: itemWidth},800);
     });
-    
-    /* Bootstrap Tooltip for Skillset */
-    $('.level-label').tooltip();
-    
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "Francisjhacke");
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "Francisjhacke", selector: "#ghfeed" });
+  });
 
+  /* Bootstrap Tooltip for Skillset */
+  $(".level-label").tooltip();
 
+  /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
+  GitHubCalendar("#github-graph", "Francisjhacke");
+
+  /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
+  GitHubActivity.feed({ username: "Francisjhacke", selector: "#ghfeed" });
 });
